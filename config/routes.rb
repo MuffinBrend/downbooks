d175c7b9e7040d3e8573bad4553f252d7081eadc
MuffinBrend <brenda.alvarez.jimenez@gmail.com>
2015-11-13 22:29:53 +0100

    add startpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'startpage#index'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ test/controllers/startpage_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StartpageControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
