6b1ba4c075c93f21ba0b5c30e2f2f1606e2aa369
MuffinBrend <brenda.alvarez.jimenez@gmail.com>
2015-11-14 19:42:41 +0100

    add edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,44 @@ class UsersController < ApplicationController
   def create
     newUser = params[:user]
     @user = User.new(username: newUser[:username], name: newUser[:name], email: newUser[:email], password: newUser[:password])
-    if @user.save then
+    if @user.save
       flash[:success] = "Usuario #{@user.username} creado con éxito!"
       redirect_to users_path
     else
       flash.now[:error] = "Ocurrió un error creando el usuario #{params[:user][:username]}"
       render action: 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    editedUser = params[:user]
+    begin
+      @user = User.find(params[:id])
+      if @user.authenticate(editedUser[:current_password])
+        @user.name = editedUser[:name]
+        @user.email = editedUser[:email]
+        if not editedUser[:new_password].empty?
+          @user.password = editedUser[:new_password]
+          puts "lol #{editedUser[:new_password]}"
+        end
+        if @user.save
+          flash[:success] = "Usuario #{@user.username} editado con éxito!"
+          redirect_to @user
+        else
+          flash.now[:error] = "Ocurrió un error editando el usuario #{@user.username}"
+          render action: 'edit'
+        end
+      else
+        flash.now[:error] = "La contraseña no es correcta"
+        render action: 'edit'
+      end
+    rescue Exception
+      flash[:error] = "No se pudo encontrar el usuario"
+      redirect_to users_path
     end
   end
 end
