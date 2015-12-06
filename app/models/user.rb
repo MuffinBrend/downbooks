b98b67d1e012a42ee41c0e98260598f342c3e797
MuffinBrend <brenda.alvarez.jimenez@gmail.com>
2015-12-06 01:03:11 +0100

    Changed error message to spanish

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 150}
   validates :username, presence: true, length: {maximum: 25}, uniqueness: {case_sensitive: false}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: 8}, if: :password_digest_changed?
+  validates :password, length: {minimum: 8, too_short: "demasiado corta"}, if: :password_digest_changed?
 
   has_secure_password
 
