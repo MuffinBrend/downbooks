af5e156a707dd9201aab2d9551e16662cc8b8290
MuffinBrend <brenda.alvarez.jimenez@gmail.com>
2015-12-06 16:39:12 +0100

    Added recent added books and users to startpage logged in
    fixes #4

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -2,9 +2,10 @@ class StartpageController < ApplicationController
 
   def index
     if logged_in?
-      @books = []
-      Rating.order(rating: :desc).limit(6).each{|r| @books.push(r.book)}
-
+      @top_books = []
+      Rating.order(rating: :desc).limit(6).each{|r| @top_books.push(r.book)}
+      @recent_books = Book.order(created_at: :desc).limit(6)
+      @random_users = User.where.not(id: current_user.id).order(username: :desc).sample(6)
       render 'startpage/logged_in_index', layout: 'layouts/application'
     end
   end
